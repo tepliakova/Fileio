2aab2ad460d31f84f2462ea66a01ce94713d40a5
Proba <teplyakovateplyakova94@gmail.com>
2024-10-24 22:52:55 +1000

    Заготовка проекта с функцией upload для загрузки изображения на сайт file.io

---
+++ main.py
@@ -0,0 +1,30 @@
+from http.client import responses
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import ttk
+import requests
+
+
+def upload():
+    filepath = fd.askopenfilename()
+    if filepath:
+        files = {'file': open(filepath, 'rb')}
+        response = requests.post('https://file.io', files = files)
+        if response.status_code == 200:
+            link = response.json()['link']
+            entry.insert(0, link)
+
+
+
+
+window = Tk()
+window.title("Сохранение файла в облаке")
+window.geometry("400x200")
+
+button = ttk.Button(text="Загрузить файл", command=upload)
+button.pack()
+
+entry = ttk.Entry()
+entry.pack()
+
+window.mainloop()
